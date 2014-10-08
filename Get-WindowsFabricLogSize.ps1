66d749964466aa5cb4dbc9d7f8ecae6816974d68
Tom Arbuthnot <tom@tomarbuthnot.com>
2014-10-08 21:00:09 +0100

    formatting tidy (thanks ISESteroids!)

--- Get-WindowsFabricLogSize.ps1
+++ Get-WindowsFabricLogSize.ps1
@@ -1,51 +1,67 @@
 ﻿
-# 0.1
+# 0.3
+
+# Gathers the windows fabric log file Size of a Lync 2013 Server
+# Use at your own risk. Test before using in production
 
 # Assumes windows Fabric is on C:\
 
-$VerbosePreference = "Continue"
+$VerbosePreference = 'Continue'
 
-$FinalOutput=  @()
-Get-CsPool | Where-Object {$_.Services -like '*UserServer*'} |  
+$FinalOutput = @()
+Get-CsPool |
+Where-Object  -FilterScript {
+    $_.Services -like '*UserServer*'
+} |  
 Select-Object -ExpandProperty Computers | 
-ForEach-Object {
-        Write-Verbose "Testing $_"
+ForEach-Object  -Process {
+        Write-Verbose  -Message "Testing $_"
         $TestPath = $null
         $TestPath2 = $null
-        # Note, no windows Fabric on 2010 Poool, Test_Path Coveres this
+        # Note, No Windows Fabric on 2010 Poool, Test_Path Coveres this, since the path does not exist on a 2010 server
         $Path1 = "\\$_\C$\ProgramData\Windows Fabric\Fabric\log\Traces"
-        $TestPath = Test-Path "$Path1"
-        Write-Verbose "Test Path 1 Result: $TestPath" 
+        $TestPath = Test-Path  -Path "$Path1"
+        Write-Verbose  -Message "Test Path 1 Result: $TestPath" 
         
         # Alternate Path
         $Path2 = "\\$_\c$\ProgramData\Windows Fabric\Log\Traces"
-        $TestPath2 = Test-Path "$path2"
-        Write-Verbose "Test Path 2 Result: $TestPath2" 
+        $TestPath2 = Test-Path  -Path "$path2"
+        Write-Verbose  -Message "Test Path 2 Result: $TestPath2" 
 
         If ($TestPath)
             {
-            $Size = Get-ChildItem "$Path1" | Measure-Object -property length -sum
+            $Size = Get-ChildItem  -Path "$Path1" | Measure-Object -Property length -Sum
             $Path = $Path1
-            }
+
+            $FolderSizeMB = $Size.sum / 1MB
+            $FolderSizeMB = [Math]::Round($FolderSizeMB,2)
+            Write-Verbose  -Message "Server $_"
+            Write-Verbose  -Message "Path $Path"
+            Write-Verbose  -Message "Size $FolderSizeMB MB"
+
+            $output = New-Object -TypeName PSobject 
+            $output | Add-Member  -MemberType NoteProperty  -Name 'Computer' -Value $_
+            $output | Add-Member  -MemberType NoteProperty  -Name 'WinFabricLogSizeMB' -Value $FolderSizeMB
+            $FinalOutput += $output
+    }
         
         If ($TestPath2)
             {
-            $Size = Get-ChildItem "$Path2" | Measure-Object -property length -sum
-            $Path = $path2
-            }
+            $Size = Get-ChildItem  -Path "$Path2" | Measure-Object -Property length -Sum
+            $Path = $Path2
             
-            $FolderSizeMB = $size.sum / 1MB
+            $FolderSizeMB = $Size.sum / 1MB
             $FolderSizeMB = [Math]::Round($FolderSizeMB,2)
-            Write-Verbose "Server $_"
-            Write-Verbose "Path $Path"
-            Write-Verbose "Size $FolderSizeMB MB"
+            Write-Verbose  -Message "Server $_"
+            Write-Verbose  -Message "Path $Path"
+            Write-Verbose  -Message "Size $FolderSizeMB MB"
 
-                    $output = New-Object -TypeName PSobject 
-                    $output | add-member NoteProperty "Computer" -value $_
-                    $output | add-member NoteProperty "WinFabricLogSizeMB" -value $FolderSizeMB
-                    $FinalOutput += $output
-
-                } # Close Foreach-Object
+            $output = New-Object -TypeName PSobject 
+            $output | Add-Member  -MemberType NoteProperty  -Name 'Computer' -Value $_
+            $output | Add-Member  -MemberType NoteProperty  -Name 'WinFabricLogSizeMB' -Value $FolderSizeMB
+            $FinalOutput += $output
+    }
+} # Close Foreach-Object
 
 
 $FinalOutput | Format-Table -AutoSize
